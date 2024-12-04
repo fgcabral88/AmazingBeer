75d92a23ce6af161b868cf637f037cc8ea20c9b0
Felipe <fgcabral@yahoo.com>
2024-12-04 17:02:55 -0300

    #feat: Parte 2

---
+++ AmazingBeer.Api/Infraestructure/Mappers/CervejaProfile.cs
@@ -0,0 +1,17 @@
+﻿using AmazingBeer.Api.Application.Dtos.Cerveja;
+using AmazingBeer.Api.Domain.Entitys;
+using AutoMapper;
+
+namespace AmazingBeer.Api.Infraestructure.Mappers
+{
+    public class CervejaProfile : Profile
+    {
+        public CervejaProfile()
+        {
+            // Entidade para Dto | Dto para Entidade - Cerveja
+            CreateMap<CervejaEntity, ListarCervejaDto>().ReverseMap();
+            CreateMap<CervejaEntity, CriarCervejaDto>().ReverseMap();
+            CreateMap<CervejaEntity, EditarCervejaDto>().ReverseMap();
+        }
+    }
+}
