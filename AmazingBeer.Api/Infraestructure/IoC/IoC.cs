3671686cd8fe04b14e60850811a75065ae334b6f
Felipe <fgcabral@yahoo.com>
2024-12-05 12:48:18 -0300

    #feat: Parte 1

---
+++ AmazingBeer.Api/Infraestructure/IoC/IoC.cs
@@ -0,0 +1,49 @@
+﻿using AmazingBeer.Api.Application.Behaviors.Validations;
+using AmazingBeer.Api.Application.Interfaces;
+using AmazingBeer.Api.Application.Services;
+using AmazingBeer.Api.Domain.Interfaces;
+using AmazingBeer.Api.Infraestructure.Data.Repositories;
+using AmazingBeer.Api.Infraestructure.Mappers;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Serilog;
+
+namespace AmazingBeer.Api.Infraestructure.IoC
+{
+    public static class IoC
+    {
+        public static IServiceCollection AdicionarDependencias(this IServiceCollection services)
+        {
+            // Registrar Serviços
+            services.AddScoped<ICervejaService, CervejaService>();
+
+            // Registrar Repositórios
+            services.AddScoped<ICervejaRepository, CervejaRepository>();
+
+            #region Registar Outros Serviços
+
+            // FluentValidation
+            services.AddFluentValidationAutoValidation();
+            services.AddFluentValidationClientsideAdapters();
+            services.AddValidatorsFromAssemblyContaining<CriarCervejaDtoValidation>();
+
+            // AutoMapper
+            services.AddAutoMapper(typeof(CervejaProfile));
+
+            // Razor
+            services.AddRazorPages();
+
+            // Serilog
+            services.AddSerilog();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration: services.BuildServiceProvider()
+                .GetService<IConfiguration>() ?? new ConfigurationBuilder().Build())
+                .CreateLogger();
+
+            #endregion
+
+            return services;
+        }
+    }
+}
